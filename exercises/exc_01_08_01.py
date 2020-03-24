58553de6e4099e02bf5b3c849e60e92fa341eda4
attwaben <atto_ben@yahoo.com>
2020-03-24 12:01:57 +0100

    Update exc_01_08_01.py

--- exercises/exc_01_08_01.py
+++ exercises/exc_01_08_01.py
@@ -5,12 +5,12 @@ nlp = spacy.load("en_core_web_sm")
 text = "It’s official: Apple is the first U.S. public company to reach a $1 trillion market value"
 
 # Process the text
-doc = ____
+doc = nlp(text)
 
 for token in doc:
     # Get the token text, part-of-speech tag and dependency label
-    token_text = ____.____
-    token_pos = ____.____
-    token_dep = ____.____
+    token_text = token.text
+    token_pos = token.pos_
+    token_dep = token.dep_
     # This is for formatting only
     print("{:<12}{:<10}{:<10}".format(token_text, token_pos, token_dep))
