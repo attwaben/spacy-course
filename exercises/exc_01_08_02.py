a67e2f1bbcae6cb4b837d719455b85584e6b8560
attwaben <atto_ben@yahoo.com>
2020-03-24 12:21:33 +0100

    Update exc_01_08_02.py

--- exercises/exc_01_08_02.py
+++ exercises/exc_01_08_02.py
@@ -5,9 +5,9 @@ nlp = spacy.load("en_core_web_sm")
 text = "It’s official: Apple is the first U.S. public company to reach a $1 trillion market value"
 
 # Process the text
-doc = ____
+doc = nlp(text)
 
 # Iterate over the predicted entities
-for ent in ____.____:
+for ent in doc.ents:
     # Print the entity text and its label
-    print(ent.____, ____.____)
+    print(ent.text, ent.label_)
