5c07beb5ab7a48933357d9340bd4972fc8f28f96
attwaben <atto_ben@yahoo.com>
2020-03-24 12:32:11 +0100

    Update exc_01_09.py

--- exercises/exc_01_09.py
+++ exercises/exc_01_09.py
@@ -5,15 +5,15 @@ nlp = spacy.load("en_core_web_sm")
 text = "New iPhone X release date leaked as Apple reveals pre-orders by mistake"
 
 # Process the text
-doc = ____
+doc = nlp(text)
 
 # Iterate over the entities
-for ____ in ____.____:
+for ent in doc.ents:
     # Print the entity text and label
-    print(____.____, ____.____)
+    print(ent.text, ent.label_)
 
 # Get the span for "iPhone X"
-iphone_x = ____
+iphone_x = doc[1:3]
 
 # Print the span text
 print("Missing entity:", iphone_x.text)
